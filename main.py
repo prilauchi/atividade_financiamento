571179270440e09eb83d66dab7f3345de5123364
GabrieLspLeite <gabrielleitealtern@gmail.com>
2024-08-30 18:34:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ parcelamento = int(input("Digite a quantidade de parcelas : "))
 valor_parcelamento = (valor_financiamento / parcelamento)
 
 print(f"Olá {nome_cliente} você deseja financiar o valor de: {valor_financiamento} em {parcelamento}, vai ficar o valor de : {valor_parcelamento}")
+
+valor = float(input('Digite o valor do pagamento'))
+valor_desconto = valor * 20/100
+pagamento_antecipado = valor - valor_desconto
+print(pagamento_antecipado)
