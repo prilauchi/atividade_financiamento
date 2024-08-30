fa6b4dbbeb21558ccc9d659a4a6bfabde60a1e59
GabrieLspLeite <gabrielleitealtern@gmail.com>
2024-08-30 18:34:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ tempo = int(input("Digite o tempo esperado de pagamento pelo empréstimo em anos
 valor_final = emprestimo*(1+juro*tempo)
 
 print(f"R${valor_final} é o valor total a ser pago pelo empréstimo.")
+
+nome_cliente = input("Digite seu nome : ")
+valor_financiamento = float(input("Digite o valor do financiamento estudantil : "))
+parcelamento = int(input("Digite a quantidade de parcelas : "))
+valor_parcelamento = (valor_financiamento / parcelamento)
+
+print(f"Olá {nome_cliente} você deseja financiar o valor de: {valor_financiamento} em {parcelamento}, vai ficar o valor de : {valor_parcelamento}")
